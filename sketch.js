99328c9d4d4faff82f3863bba236173e26974cb6
giucr <giulia.croce99@hotmail.com>
2024-06-07 11:04:40 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//variáveis da bolinha 
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 16;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variaveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//variaveis do oponente 
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velocidadeYOponente;
+
+let colidiu = false ;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
+//sons do jogo
